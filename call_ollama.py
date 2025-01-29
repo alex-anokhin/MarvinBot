bd19e96d03f849dc84e145be7f7a1c59d7e6f61f
pavel <plavik2@gmail.com>
2025-01-29 15:37:35 +0100

    Change function askllm to get_answer_from_ollama - use history and RAG (with RAG history, if need) in system prompt. Add flag to .env for RAG history.

--- call_ollama.py
+++ call_ollama.py
@@ -2,13 +2,14 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 import ollama
+from ollama import chat
 import sys
 load_dotenv()
 sys.path.append("sessions")
 sys.path.append("messages")
 sys.path.append("users")
 from sessions import get_sessions_list, get_session
-from messages import add_message
+from messages import add_message, get_messages
 from users import add_user
 
 def get_pages_for_history(session_id, dbconn):
@@ -23,16 +24,16 @@ def get_pages_for_history(session_id, dbconn):
     for (page_id) in dbcursor:
         query = ("""SELECT title, doc, link FROM pages WHERE id = %s""")
         dbcursor1.execute(query, (page_id,))
-        for (title, doc, link) in dbcursor1:
-            results.append([str(title), str(doc), str(link)])
+        for (title, data, link) in dbcursor1:
+            results.append("Title: {}\nData: {}\nLink: {}\n".format(title, data, link))
     dbconn.commit()
     dbcursor1.close()    
     dbcursor.fetchall()
+    dbcursor.close() 
     return(results)
 
-def get_pages_for_answer(prompt, session_id, dbconn):
+def get_pages_for_answer(prompt, message_id, dbconn):
     dbcursor = dbconn.cursor()
-    message_id = add_message(session_id, "user", prompt, dbconn)
     response = ollama.embeddings(
         prompt=prompt,
         model="mxbai-embed-large"
@@ -56,49 +57,33 @@ def get_pages_for_answer(prompt, session_id, dbconn):
     dbcursor.close()
     return(results)
 
-def askllm(prompt, dbconn, session_id = 1) -> str:
+def get_answer_from_ollama(prompt, dbconn, session_id = 1) -> str:
     # RAG
-    pages = get_pages_for_answer(prompt, session_id, dbconn)
-    # Add RAG pages for messages from history
-    for history_page in get_pages_for_history(session_id, dbconn):
-        if history_page not in pages:
-            pages.append(history_page)
-    resultstr = str()
-    for (title, data, link) in pages:
-        resultstr += "Title: {}\nData: {}\nLink: {}\n".format(title, data, link)
-    # print(resultstr)
-    ##Get history
-    # session_messages = get_session(session_id, dbconn)
-    # print("History:\n{}\n".format(session_messages))
-    ##Send question    
-    output = ollama.generate(
-        model="llama3.2",
-        prompt=f"""You are Marvin bot from The Hitchhiker's Guide to the Galaxy.
-                Using this data: {resultstr}.
-                Respond to this prompt: {prompt}.
-                Make short and clear answers, respond concisely and use emojis."""
+    message_id = add_message(session_id, "user", prompt, dbconn)
+    pages = get_pages_for_answer(prompt, message_id, dbconn)
+    # Add RAG for messages from history
+    if (os.getenv("USE_RAG_HISTORY", "False") == "True"):
+        history_pages = get_pages_for_history(session_id, dbconn)
+        pages.append(history_pages)
+##    for page in pages:
+##        print(page)
+##        print('\n')
+##    Get message history
+    session_messages = [{'role': 'system', 'content': f"""You are Marvin bot from The Hitchhiker's Guide to the Galaxy.
+                        Using this data for answer:{pages}.
+                        Make short and clear answers, respond concisely and use emojis."""}]
+    session_messages.extend(get_messages(session_id, dbconn))
+    response = chat(
+        'llama3.2',
+        messages = session_messages
         )
-#                ("Use this history of our last conversation: {history}." if len(history) > 5 else "") +
-                
-#                Show the links to data after your answer after the word 'Links:'."""
-#                And at the end of answer  after the word "History:" write short history of our conversation for our next conversation."""
-#        )
 
-#    answer = output['response'].split("History:")
-#    print("New history:\n{0}\n\n".format(answer[1]))
-#    if (len(history) > 0):
-#        query = "UPDATE history SET history=%s WHERE id=%s"
-#    else:
-#        query = "INSERT INTO history (history, id) VALUES (%s, %s)"
-#    data = (answer[1], session_id)
-#    dbcursor.execute(query, data)
-#    dbconn.commit()
-#    return(answer)
-    message_id = add_message(session_id, "assistant", output['response'], dbconn)
-    return(output['response'])
+##    message_id = add_message(session_id, "assistant", output['response'], dbconn)
+    message_id = add_message(session_id, "assistant", response['message']['content'], dbconn)
+    return(response['message']['content'])
 ##    return (output['response'])
 
-# Connect to your postgres DB
+# Connect to postgres DB
 dbconn = psycopg2.connect(host=os.getenv("DB_HOST"),
                         dbname=os.getenv("DB_NAME"),
                         user=os.getenv("DB_USER"),
@@ -109,16 +94,16 @@ curr_session_id = 0
 sessions_list = get_sessions_list("test_user@42heilbronn.de", dbconn)
 for session in sessions_list:
     curr_session_id = session[0]
-    print("Session id {} title: {}, end_time: {}".format(session[0], session[1], session[2]))
-    messages_list = get_session(session[0], dbconn)
-    for message in messages_list:
-        print("{} role {}: {}".format(message[0], message[1], message[2]))
+##    print("Session id {} title: {}, end_time: {}".format(session[0], session[1], session[2]))
+##    messages_list = get_session(session[0], dbconn)
+##    for message in messages_list:
+##        print("{} role {}: {}".format(message[0], message[1], message[2]))
 
 print("Write your question:")
 for prompt in sys.stdin:
     if (prompt.rstrip() == "exit"):
         break
-    print("Answer from llama:\n{0}\n\n".format(askllm(prompt, dbconn, curr_session_id)))
+    print("Answer from llama:\n{0}\n\n".format(get_answer_from_ollama(prompt, dbconn, curr_session_id)))
     print("Write your question:")
 
 print("Done")
--- messages/messages.py
+++ messages/messages.py
@@ -18,3 +18,16 @@ def add_message(session_id, role, content, dbconn):
     dbcursor.close()
     return (message_id)
 
+def get_messages(session_id, dbconn):
+    dbcursor = dbconn.cursor()
+    messages_list=list()
+    query = ("""SELECT messages.role, messages.content, messages.id
+            FROM messages, sessions
+            WHERE messages.session_id = sessions.id AND sessions.id = %s
+            ORDER BY messages.id ASC""")
+    dbcursor.execute(query, (session_id, ))
+    for (role, content, id) in dbcursor:        
+        messages_list.append({'role': role, 'content': content})
+##    print(messages_list)
+    dbcursor.close()
+    return(messages_list)
